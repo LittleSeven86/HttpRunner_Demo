eba1d61c7d11ee1241643f27613ab91ba5f5e38e
张猛 <kailzhang893@outlook.com>
2023-05-22 22:16:38 +0800

    添加框架pytest--case

---
+++ demo/testcases/pytestCaseDemo/testDemo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+'''
+-----------------------------------------------------------
+    @FileName  :testDemo.py
+    @Time      :2023/5/22 22:07
+    @Author    :LittleSeven
+    @Address   ：https://gitee.com/linguchong/Django_Project_Demo.git
+-----------------------------------------------------------
+'''
+import pytest
+
+def test_demo1():
+    assert 1,1
+
+
