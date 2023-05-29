175c4338f7ae88bc9dba04dc656d8e39f8d1430c
张猛 <kailzhang893@outlook.com>
2023-05-29 22:42:19 +0800

    更新至接口validate部分

--- run-test.py
+++ run-test.py
@@ -2,10 +2,13 @@
 # -*- coding=utf-8 -*-
 """
 @Time    :  2022-09-13 21:50
-@Author  :  王彦青
 @File    :  main-runner-hapi.py
 """
+import datetime
 from httprunner.cli import main_run
+
+now = datetime.datetime.now()
+
 if __name__ == '__main__':
 
     # -v 参数：debug模式打印出具体执行过程和请求响应出入参
@@ -14,12 +17,11 @@ if __name__ == '__main__':
     #       ])
 
 # 可直接运行，如果想定位问题，可直接debug模式运行
-    main_run([r"D:\Python\Httprunner_Demo\demo\testcases\ymlCaseDemo\parameters_csv.yml",  # case路径
+    main_run([r"/Users/z.m/pythonProject/HttpRunner_demo/demo/testcases/ymlCaseDemo/Yml_demo.yml",  # case路径
               '-v',
               # '-s',
               # '--report=hhhhhh.html',
-              '--alluredir=reports/7'
-              '【HttpRunner】自动化报告',
+              f'--alluredir=reports/{now.strftime("%Y:%m:%d %H:%M:%S")}【HttpRunner】自动化报告',
               # '--tester=【酒旅-质量保障部】',
               # '--desc=报告描述信息【酒旅-质量保障部】',
               #'--template=2'
